780e84d5dac10198f3bf3f5c4ddf4537ff7316d0
cjpalacios07 <88065795+cjpalacios07@users.noreply.github.com>
2021-08-21 12:57:36 -0400

    HI

---
+++ script.js
@@ -0,0 +1,13 @@
+ <tr>
+                        <td>Doggo</td>
+                        <td id="doggo-votes">0</td>
+                    </tr>
+                    <tr>
+                        <td>Catto</td>
+                        <td id="catto-votes">0</td>
+                    </tr>
+                    <tr>
+                        <td>Fish Gold</td>
+                        <td id="fish-votes"
+
+
